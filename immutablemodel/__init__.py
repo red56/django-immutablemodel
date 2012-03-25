0b71295996e24845de6af092b5f104533f3f2d67
Tim Diggins <tim@red56.co.uk>
2012-03-25 09:09:16 +0100

    exposing a couple of constants to make config clearer

--- immutablemodel/__init__.py
+++ immutablemodel/__init__.py
@@ -6,5 +6,5 @@ Copyright:
 Please see LICENSE and AUTHORS for more information.
 """
 
-from models import ImmutableModel, CantDeleteImmutableException
+from models import ImmutableModel, CantDeleteImmutableException, PK_FIELD, UNDEFINED
 from admin import ImmutableModelAdmin
